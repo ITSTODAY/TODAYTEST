e4756e22e92894f7cfc31682b817020cf2daf778
马天翼 <matianyi@matianyideMacBook-Pro.local>
2017-06-28 21:56:50 +0800

    Initial Commit

---
+++ TODAY/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TODAY
+//
+//  Created by 马天翼 on 2017/6/28.
+//  Copyright © 2017年 TODAY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
